1e099beef74df37514cb9731ee63617ab541cd3a
Daniel Bergey <daniel.bergey@braze.com>
2022-12-11 09:58:29 -0500

    quantifiers, equivalence up to spans

--- src/parser.rs
+++ src/parser.rs
@@ -1,12 +1,13 @@
 extern crate nom;
 
-use nom::character::complete::{char, none_of, one_of};
+use nom::character::complete::{char, none_of, one_of, u32};
 use nom::branch::alt;
 use nom::{
     multi::many1,
+    combinator::opt,
     Finish, IResult,
 };
-use regex_syntax::ast::{Ast, Concat, Literal, LiteralKind, Position, Span};
+use regex_syntax::ast::{Ast, Concat, Literal, LiteralKind, Position, Repetition, RepetitionKind, RepetitionOp, RepetitionRange, Span};
 
 type Progress<'a> = IResult<&'a str, Ast>;
 
@@ -60,8 +61,56 @@ fn atom(s: &str) -> Progress {
     alt((literal, escaped_literal, dot))(s)
 }
 
+fn char_quantifier(s: &str) -> IResult<&str, RepetitionKind> {
+    let (s, c) = one_of("*+?")(s)?;
+    let quantifier = match c {
+        '*' => RepetitionKind::ZeroOrMore,
+        '+' => RepetitionKind::OneOrMore,
+        '?' => RepetitionKind::ZeroOrOne,
+        _ => panic!("one_of returned an unexpected character")
+
+    };
+    Ok((s, quantifier))
+}
+
+fn bound(s: &str) -> IResult<&str, RepetitionKind> {
+    let (s, _) = char('{')(s)?;
+    let (s, min) = u32(s)?;
+    let (s, o_comma) = opt(char(','))(s)?;
+    match o_comma {
+        None => Ok((s, RepetitionKind::Range(RepetitionRange::Exactly(min)))),
+        Some(_) => {
+            let (s, o_max) = opt(u32)(s)?;
+            match o_max {
+                None => Ok((s, RepetitionKind::Range(RepetitionRange::AtLeast(min)))),
+                // check that max <= 255 to match sed?
+                Some(max) => Ok((s, RepetitionKind::Range(RepetitionRange::Bounded(min, max))))
+            }
+        }
+    }
+}
+
+fn quantified_piece(s: &str) -> Progress {
+    let (s, atom) = atom(s)?;
+    let (s, o_quantifier) = opt(alt((char_quantifier, bound)))(s)?;
+    match o_quantifier {
+        None => Ok((s, atom)),
+        Some(quantifier) => {
+            Ok((s, Ast::Repetition(Repetition {
+                span: ZERO_SPAN,
+                op: RepetitionOp {
+                    span: ZERO_SPAN,
+                    kind: quantifier
+                },
+                greedy: true,
+                ast: Box::new(atom)
+            })))
+        }
+    }
+}
+
 fn branch(s: &str) -> Progress {
-    let (s, atoms) = many1(atom)(s)?;
+    let (s, atoms) = many1(quantified_piece)(s)?;
     Ok((s, Ast::Concat(Concat{
         span: ZERO_SPAN,
         asts: atoms
@@ -75,10 +124,69 @@ pub fn posix(s: &str) -> Result<Ast, nom::error::Error<&str>> {
     Ok(ast)
 }
 
+mod equivalent {
+  use regex_syntax::ast::*;
+  // compare regex ASTs, ignoring Span & Position
+  pub trait Equivalent {
+      fn equivalent(&self, _: &Self) -> bool;
+  }
+
+  impl Equivalent for Ast {
+      fn equivalent(&self, other: &Ast) -> bool {
+          match (self, other) {
+              (Ast::Empty(_), Ast::Empty(_)) => true,
+              (Ast::Flags(_), Ast::Flags(_)) => panic!("not implemented"),
+              (Ast::Literal(a), Ast::Literal(b)) => a.equivalent(b),
+              (Ast::Dot(_), Ast::Dot(_)) => true,
+              (Ast::Assertion(_), Ast::Assertion(_)) => panic!("not implemented"),
+              (Ast::Class(_), Ast::Class(_)) => panic!("not implemented"),
+              (Ast::Repetition(a), Ast::Repetition(b)) => a.equivalent(b),
+              (Ast::Group(_), Ast::Group(_)) => panic!("not implemented"),
+              (Ast::Alternation(_), Ast::Alternation(_)) => panic!("not implemented"),
+              (Ast::Concat(a), Ast::Concat(b)) => a.equivalent(b),
+              _ => false
+          }
+      }
+  }
+
+  impl Equivalent for Literal {
+      fn equivalent(&self, other: &Literal) -> bool {
+          self.c == other.c && self.kind == other.kind
+      }
+  }
+
+  impl Equivalent for Repetition {
+      fn equivalent(&self, other: &Repetition) -> bool {
+          self.op.equivalent(&other.op) && self.greedy == other.greedy && self.ast.equivalent(&other.ast)
+      }
+  }
+
+  impl Equivalent for RepetitionOp {
+      fn equivalent(&self, other: &RepetitionOp) -> bool {
+          self.kind == other.kind
+      }
+  }
+
+  impl Equivalent for Concat {
+      fn equivalent(&self, other: &Concat) -> bool {
+          self.asts.len() == other.asts.len() && std::iter::zip(&self.asts, &other.asts).all(|(a, b)| a.equivalent(&b))
+      }
+  }
+}
+
 #[cfg(test)]
 pub mod tests {
-    use crate::*;
+    use super::equivalent::Equivalent;
+    use super::*;
     use assert_ok::assert_ok;
+    use regex_syntax::ast::parse::Parser;
+
+
+    fn match_modern_syntax(pattern: &str) {
+        let expected = Parser::new().parse(pattern).unwrap();
+        let actual = assert_ok!(posix(pattern));
+        assert!(actual.equivalent(&expected));
+    }
 
     #[test]
     fn literals() {
@@ -103,5 +211,10 @@ pub mod tests {
             },
             _ => panic!("unexpected regex parse: {:?}", ast),
         }
+    }
+
+    #[test]
+    fn star() {
+        match_modern_syntax("foo*");
     }
 }
