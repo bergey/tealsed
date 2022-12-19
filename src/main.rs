97a8d0821dc1f2fd3051e568ed2efb0de13f08d5
Daniel Bergey <daniel.bergey@braze.com>
2022-12-18 21:21:08 -0500

    accept file arguments, loop

--- src/main.rs
+++ src/main.rs
@@ -19,27 +19,7 @@ struct Cli {
     no_print: bool,
 }
 
-fn main() -> io::Result<()> {
-    let args = Cli::parse();
-
-    let stdin = io::stdin();
-
-
-    let commands: Vec<Command> =
-        if args.commands.len() == 0 {
-            match args.command_or_files.first() {
-                Some(arg) => parse_command_finish(&arg)
-                    .map(|cmd| Vec::from([cmd]))?,
-                None => Vec::new()
-            }
-        } else {
-            args.commands.iter()
-                .map(|cmd| parse_command_finish(&cmd))
-                .collect::<io::Result<Vec<Command>>>()?
-        };
-
-    // TODO files
-
+fn run_commands<R>(commands: &[Command], mut input: R, no_print: bool) -> io::Result<()> where R: io::BufRead {
     // keep reusing these buffers
     let mut buf = String::new();
     let mut line_number = 0;
@@ -47,10 +27,10 @@ fn main() -> io::Result<()> {
     let mut read = String::new();
     let mut write = String::new();
     let mut in_matching_range = Vec::with_capacity(commands.len());
-    for _ in &commands {
+    for _ in commands {
         in_matching_range.push(false);
     }
-    while stdin.read_line(&mut buf)? != 0 {
+    while input.read_line(&mut buf)? != 0 {
         line_number += 1;
         read.clear();
         read.push_str(&buf);
@@ -90,8 +70,46 @@ fn main() -> io::Result<()> {
                 }
             }
         }
-        if !args.no_print { print!("{}", read); }
+        if !no_print { print!("{}", read); }
         buf.clear();
     }
+    Ok(())
+}
+
+fn main() -> io::Result<()> {
+    let args = Cli::parse();
+
+
+    let commands: Vec<Command> =
+        if args.commands.len() == 0 {
+            match args.command_or_files.first() {
+                Some(arg) => parse_command_finish(&arg)
+                    .map(|cmd| Vec::from([cmd]))?,
+                None => Vec::new()
+            }
+        } else {
+            args.commands.iter()
+                .map(|cmd| parse_command_finish(&cmd))
+                .collect::<io::Result<Vec<Command>>>()?
+        };
+
+    let file_args = if args.commands.len() == 0 {
+        &args.command_or_files[1..]
+    } else {
+        &args.command_or_files
+    };
+
+    if file_args.len() == 0 {
+        let stdin = io::stdin();
+        let mut handle = stdin.lock();
+        run_commands(&commands, &mut handle, args.no_print)?;
+    } else {
+        for filename in file_args {
+            let file = std::fs::File::open(filename)?;
+            let mut buf_reader = io::BufReader::new(file);
+            run_commands(&commands, &mut buf_reader, args.no_print)?;
+        }
+    }
+
     Ok(())
 }
